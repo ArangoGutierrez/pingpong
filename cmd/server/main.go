8f3d03738ac409a21fab5225ee7786e574c78067
Eduardo Arango <eduardo@sylabs.io>
2019-02-07 18:57:12 -0500

    Update main.go

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -18,6 +18,7 @@ type PongServer struct {     }
 func (ps *PongServer) PingPongRPC(stream pb.PongService_PingPongRPCServer) error {
 	log.Println("Started stream")
 	
+	
 	for {
 		in, err := stream.Recv()
 		log.Println("Ping ...>  %i", in.Ball)
